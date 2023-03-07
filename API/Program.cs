bd68ab261561bb5f95ed4081226326de7183b91c
lurulu156 <kzzlin9@gmail.com>
2023-03-07 18:54:24 +0800

    feat: add exception middleware

--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,5 @@
 using API.Extensions;
+using API.Middleware;
 using Application.Core;
 using Application.Scenarios;
 using MediatR;
@@ -16,6 +17,7 @@ builder.Services.AddApplicationServices(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseMiddleware<ExceptionMiddleware>();
 if (app.Environment.IsDevelopment())
 {
   app.UseSwagger();
