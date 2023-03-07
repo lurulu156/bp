90f3fb80cb6912f7c5cb9ca094053ed78bbc1351
lurulu156 <kzzlin9@gmail.com>
2023-03-07 16:54:13 +0800

    refactor: service extensions

--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,4 @@
+using API.Extensions;
 using Application.Core;
 using Application.Scenarios;
 using MediatR;
@@ -9,22 +10,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<DataContext>(opt =>
-{
-  opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
-builder.Services.AddCors(opt =>
-{
-  opt.AddPolicy("CorsPolicy", policy =>
-  {
-    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
-  });
-});
-builder.Services.AddMediatR(typeof(List.Handler));
-builder.Services.AddAutoMapper(typeof(MappingProfiles).Assembly);
+
+builder.Services.AddApplicationServices(builder.Configuration);
 
 var app = builder.Build();
 
