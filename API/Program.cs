716bb6f31b6a36636498ddb1e7fe2ba84de846e4
lurulu156 <kzzlin9@gmail.com>
2023-03-14 15:04:56 +0800

    feat: add comment hub controller and settings

--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,6 @@
 using API.Extensions;
 using API.Middleware;
+using API.SignalR;
 using Domain;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
@@ -37,6 +38,7 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
+app.MapHub<ChatHub>("/chat");
 
 //setting up for db auto mirgration
 using var scope = app.Services.CreateScope();
